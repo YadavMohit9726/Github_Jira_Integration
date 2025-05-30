fe293c098e180e980e6687b20b7df5d5b1cd08c8
Mohit Yadav <164593784+YadavMohit9726@users.noreply.github.com>
2025-05-30 23:55:42 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,69 @@
+# python3 Github_JIRA_project.py       ---> It will run this program and the flask app
+# If it runs on local OR EC2 then it will create the jira issue via hitting URL: http://<public_ip>:5000/createJira
+# e.g: localhost:5000/createJira (if local)
+
+# This code sample uses the 'requests' library:
+# http://docs.python-requests.org
+import requests
+from requests.auth import HTTPBasicAuth
+import json
+from flask import Flask
+
+app = Flask(__name__)
+
+# Define a route that handles GET requests
+@app.route('/createJira', methods=['POST'])
+def createJira():
+
+    url = "https://dayarabugyal2k23.atlassian.net/rest/api/3/issue"
+
+    API_TOKEN="***"
+
+    auth = HTTPBasicAuth("", API_TOKEN)
+
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json"
+    }
+
+    payload = json.dumps( {
+        "fields": {
+        "description": {
+            "content": [
+                {
+                    "content": [
+                        {
+                            "text": "Order entry fails when selecting supplier.",
+                            "type": "text"
+                        }
+                    ],
+                    "type": "paragraph"
+                    }
+                ],
+            "type": "doc",
+             "version": 1
+        },
+        "project": {
+           "key": "AB"
+        },
+        "issuetype": {
+            "id": "10006"
+        },
+        "summary": "Main order flow broken",
+    },
+    "update": {}
+    } )
+
+
+    response = requests.request(
+        "POST",
+        url,
+        data=payload,
+        headers=headers,
+        auth=auth
+    )
+
+    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
